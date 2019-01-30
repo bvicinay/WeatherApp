dacc193776485a79f6486887f5cdab70bb1175cd
Borja <bvicinay@gmail.com>
2019-01-29 22:47:49 -0500

    fixed async task inconsistencies

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -23,23 +23,26 @@ public class MainActivity extends AppCompatActivity {
     private TextView weatherDescription;
     private TextView weatherTemp;
 
+    private String[] lastResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weatherDescription = (TextView)findViewById(R.id.w_description);
+        weatherTemp = (TextView)findViewById(R.id.w_temp);
 
         GetWeatherTask retrieveWeather = new GetWeatherTask();
         retrieveWeather.execute("Atlanta");
 
-        weatherDescription = (TextView)findViewById(R.id.w_description);
-        weatherTemp = (TextView)findViewById(R.id.w_temp);
+
 
 
 
     }
 
 
-    private class GetWeatherTask extends AsyncTask<String, Void, String> {
+    private class GetWeatherTask extends AsyncTask<String, Void, String[]> {
         private String URL;
 
 
@@ -49,55 +52,47 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(String... strings) {
+        protected String[] doInBackground(String... strings) {
+            String description = "";
+            String temp = "";
             try {
                 URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q="+strings[0]+"&units=imperial&APPID=726a45e6c2ce2485aa55c8e3a46dfa04");
                 HttpURLConnection con = (HttpURLConnection) url.openConnection();
                 con.setRequestMethod("GET");
-                //Object raw = con.getContent();
-                //String rep = raw.toString();
                 con.connect();
 
-
-               /* BufferedReader in = new BufferedReader(
-                        new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuffer content = new StringBuffer();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();*/
-                // Convert to a JSON object to print data
                 JsonParser jp = new JsonParser(); //from gson
                 JsonElement root = jp.parse(new InputStreamReader((InputStream) con.getContent())); //Convert the input stream to a json element
                 JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
                 // Desscription
                 JsonArray weather_data = rootobj.get("weather").getAsJsonArray();//.getAsJsonObject().get("0").getAsString();//.get("description").getAsString();//.getValues().get("description").getAsString(); //just grab the zipcode
-                String description = weather_data.get(0).getAsJsonObject().get("description").getAsString();
+                description = weather_data.get(0).getAsJsonObject().get("description").getAsString();
                 Log.d(TAG, description+" ");
 
                 //Temp
                 JsonObject main_data = rootobj.get("main").getAsJsonObject();//.getAsJsonObject().get("0").getAsString();//.get("description").getAsString();//.getValues().get("description").getAsString(); //just grab the zipcode
-                String temp = main_data.get("temp").getAsString();
+                temp = main_data.get("temp").getAsString();
                 Log.d(TAG, description+" ");
 
 
-                weatherDescription.setText(description);
-                weatherTemp.setText(temp+" F");
+
 
             } catch (Exception e) {
                 Log.d(TAG, "Error occured" + e.getLocalizedMessage());
                 //printException(e);
 
             }
-            return "";
+            lastResult =  new String[] {temp, description};
+            return lastResult;
 
 
         }
 
         @Override
-        protected void onPostExecute(String temp) {
+        protected void onPostExecute(String[] result) {
             Log.d(TAG, "result occured");
+            weatherDescription.setText(result[1]);
+            weatherTemp.setText(result[0]+" F");
         }
     }
     private void printException(Exception e) throws Exception{
